2be4824810046c60dc838ca8a7743c651733713f
bibek469 <bibek.roniyar@gmail.com>
2022-10-04 00:01:24 +0530

    Updated secrets driver

--- terraform/aws/modules/eks/kms-policy.tf
+++ terraform/aws/modules/eks/kms-policy.tf
@@ -1,17 +1,32 @@
-data "aws_iam_policy_document" "kms" {
-  statement {
-    sid = ""
+# data "aws_iam_policy_document" "kms" {
+#   statement {
+#     sid = ""
 
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ListGrants",
-      "kms:DescribeKey"
-    ]
+#     actions = [
+#       "kms:Encrypt",
+#       "kms:Decrypt",
+#       "kms:ListGrants",
+#       "kms:DescribeKey"
+#     ]
 
-    resources = [
-      "arn:aws:s3:::*",
-      "arn:aws:kms:${var.aws_region}::key:*"
-    ]
-  }
-}
+#     resources = ["*"]
+#   }
+# }
+
+
+# data "aws_iam_policy_document" "kms" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+#       type = "AWS"
+#     }
+#     actions = [
+#       "kms:Encrypt",
+#       "kms:Decrypt",
+#       "kms:ListGrants",
+#       "kms:DescribeKey"
+#     ]
+#     resources = ["*"]
+#   }
+# }
