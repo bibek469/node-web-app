a845814f823f0da59408c82694a9db571e337825
bibek469 <bibek.roniyar@gmail.com>
2022-10-04 00:15:37 +0530

    Added cluster output

---
+++ terraform/aws/env/qa/outputs.tf
@@ -0,0 +1,10 @@
+
+output "cluster_name" {
+  value = module.ping-pong-cluster.cluster_name
+  description = "Name of the cluster"
+}
+
+output "cluster_endpoint" {
+  value = module.ping-pong-cluster.cluster_endpoint
+  description = "Endpoint for your Kubernetes API server."
+}
